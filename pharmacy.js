f913774326bc78c87d08d51fdc393ab00e5f1be8
Joseph Larue <josephlarue@MacBook-Pro-de-Joseph-2.local>
2025-04-14 11:04:39 +0200

    Refactorisation de ma version de pharmacy.js, ajout de logs si une drogue a une valeur de benefit qui irait au-delà de 50 ou en-dessous de 0, ajout de tests.

--- pharmacy.js
+++ pharmacy.js
@@ -14,72 +14,38 @@ export class Pharmacy {
 updateBenefitValue() {
   const updateBenefit = (drug, benefitVariance) => {
     const newBenefit = drug.benefit + benefitVariance;
+    if (newBenefit > 50) { console.log(`New Benefit would exceeds maximum limit of 50 for ${drug.name}`); }
+    if (newBenefit < 0) { console.log(`New Benefit would be below minimum limit of 0 for ${drug.name}`); }
     const cappedBenefit = Math.min(50, newBenefit);
     drug.benefit = Math.max(0, cappedBenefit);
   };
 
   const processHerbalTea = (drug) => {
-    if (drug.expiresIn <= 0) 
-    {
-      updateBenefit(drug, 2);
-      drug.expiresIn -= 1;
-    }
-    else
-    {
-      updateBenefit(drug, 1);
-      drug.expiresIn -= 1;
-    }
+    if (drug.expiresIn <= 0) { updateBenefit(drug, 1); }
+    updateBenefit(drug, 1);
+    drug.expiresIn -= 1;
   };
 
   const processFervex = (drug) => {
-    if (drug.expiresIn > 10)
-    {
-      updateBenefit(drug, 1);
-      drug.expiresIn -= 1;
-    } 
-    else if (drug.expiresIn > 5)
-    {
-      updateBenefit(drug, 2);
-      drug.expiresIn -= 1;
-    } 
-    else if (drug.expiresIn > 0)
-    {
-      updateBenefit(drug, 3);
-      drug.expiresIn -= 1;
-    } 
-    else
-    {
-      updateBenefit(drug, -drug.benefit);
-      drug.expiresIn -= 1;
-    } 
+    if (drug.expiresIn > 10) { updateBenefit(drug, 1); } 
+    else if (drug.expiresIn > 5) { updateBenefit(drug, 2); } 
+    else if (drug.expiresIn > 0) { updateBenefit(drug, 3); } 
+    else { updateBenefit(drug, -drug.benefit); }
+    drug.expiresIn -= 1;
   };
 
   const processMagicPill = () => {};
 
   const processOtherDrugs = (drug) => {
-    if (drug.expiresIn <= 0)
-    {
-      updateBenefit(drug, -2);
-      drug.expiresIn -= 1;
-    }
-    else
-    {
-      updateBenefit(drug, -1);
-      drug.expiresIn -= 1;
-    }
+    if (drug.expiresIn <= 0) { updateBenefit(drug, -2); }
+    else { updateBenefit(drug, -1); }
+    drug.expiresIn -= 1;
   };
   
   const processDafalgan = (drug) => {
-    if (drug.expiresIn <= 0)
-    {
-      updateBenefit(drug, -4);
-      drug.expiresIn -= 1;
-    }
-    else
-    {
-      updateBenefit(drug, -2);
-      drug.expiresIn -= 1;
-    }
+    if (drug.expiresIn <= 0) { updateBenefit(drug, -4); }
+    else { updateBenefit(drug, -2); }
+    drug.expiresIn -= 1;
   };
 
   for (const drug of this.drugs) {
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -2,8 +2,8 @@ import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
   it("Test Default case", () => {
-    expect(new Pharmacy([new Drug("Randomly named Drug", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("Randomly named Drug", 1, 2)],
+    expect(new Pharmacy([new Drug("Default Drug", 2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Default Drug", 1, 2)],
     );
   });
   it("Test Dafalgan", () => {
@@ -47,4 +47,34 @@ describe("Pharmacy", () => {
     [new Drug("Magic Pill", 10, 10)],
   );
 });
+  it("Test Herbal Tea benefits don't go above MAX_BENEFIT (50)", () => {
+  expect(new Pharmacy([new Drug("Herbal Tea", 0, 49)]).updateBenefitValue()).toEqual(
+    [new Drug("Herbal Tea", -1, 50)],
+  );
+});
+  it("Test Fervex benefits don't go above MAX_BENEFIT (50)", () => {
+  expect(new Pharmacy([new Drug("Fervex", 4, 49)]).updateBenefitValue()).toEqual(
+    [new Drug("Fervex", 3, 50)],
+  );
+});
+  it("Test Dafalgan benefits don't go below MIN_BENEFIT (0)", () => {
+  expect(new Pharmacy([new Drug("Dafalgan", 0, 1)]).updateBenefitValue()).toEqual(
+    [new Drug("Dafalgan", -1, 0)],
+  );
+});
+  it("Test Default case benefits don't go below MIN_BENEFIT (0)", () => {
+    expect(new Pharmacy([new Drug("Randomly named Drug", 2, 0)]).updateBenefitValue()).toEqual(
+      [new Drug("Randomly named Drug", 1, 0)],
+    );
+  });
+    it("Test initial benefit misstype above 50", () => {
+    expect(new Pharmacy([new Drug("Misstype above 50 test", 2, 500)]).updateBenefitValue()).toEqual(
+      [new Drug("Misstype above 50 test", 1, 50)],
+    );
+  });
+    it("Test initial benefit misstype below 0", () => {
+    expect(new Pharmacy([new Drug("Misstype Below 0 test", 2, -4)]).updateBenefitValue()).toEqual(
+      [new Drug("Misstype Below 0 test", 1, 0)],
+    );
+  });
 });
